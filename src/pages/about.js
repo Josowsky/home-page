554e39c608084b8ee911a968c4a56518cf799ca7
Bartek Józwowiak <bartosz@jozwowiak.pl>
2018-06-15 18:59:11 +0200

    Initial commit with Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from '../components/header';
+
+const AboutPage = () => (
+  <div style={{ color: `teal` }}>
+    <Header/>
+    <p>Such wow. Very React.</p>
+  </div>
+);
+
+export default AboutPage;
