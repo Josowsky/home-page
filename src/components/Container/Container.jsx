ce77be77ad459bf38827938f3c0e225701a74b23
Josowsky <bartosz@jozwowiak.pl>
2019-03-08 00:23:17 +0100

    Add round edges to sidebar in Container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { func, oneOfType, node } from 'prop-types';
 
+import Sidebar from '../Sidebar/Sidebar';
+
 import styles from './container.module.css';
 
 const Container = ({ children }) => (
   <div className={styles.container}>
-    <div className={styles.sidebar}>sidevar</div>
+    <div className={styles.sidebar}>
+      <Sidebar />
+    </div>
     <div className={styles.content}>{children}</div>
   </div>
 );
