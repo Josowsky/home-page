5ce6f11ae906c2274a7f93b926fd02612bff55c4
Josowsky <bartosz@jozwowiak.pl>
2019-03-07 18:26:36 +0100

    Remove boilerplate code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,3 @@
 import React from "react"
-import { Link } from "gatsby"
 
-export default () => (
-  <div>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
-  </div>
-)
+export default () => <div>TEST</div>
