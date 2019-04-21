53bdf87bad49ea184f598e443e2621d3893c7b0c
Josowsky <bartosz@jozwowiak.pl>
2019-04-21 18:32:02 +0200

    Fix key on FameSection

--- src/components/FameSection/FameSection.jsx
+++ src/components/FameSection/FameSection.jsx
@@ -5,10 +5,12 @@ import styles from './fameSection.module.scss';
 
 const icons = [
   {
+    id: 1,
     image: <FaLinkedinIn />,
     link: 'https://www.linkedin.com/in/bartosz-j%C3%B3zwowiak-46173a114/',
   },
   {
+    id: 2,
     image: <FaRegEnvelope />,
     link: 'mailto:bartosz@jozwowiak.pl',
   },
@@ -17,9 +19,9 @@ const icons = [
 const FameSection = () => (
   <div className={styles.container}>
     {icons.map((icon, index) => (
-      <Fragment>
-        {index > 0 && <div key={index} className={styles.dot} />}
-        <a key={index} className={styles.icon} href={icon.link}>
+      <Fragment key={icon.id}>
+        {index > 0 && <div className={styles.dot} />}
+        <a className={styles.icon} href={icon.link}>
           {icon.image}
         </a>
       </Fragment>
