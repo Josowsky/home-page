206917d3205944423542124e77e39e9ea002131b
Josowsky <bartosz@jozwowiak.pl>
2019-06-21 18:32:50 +0200

    Add SimpleFooter component

---
+++ src/components/SimpleFooter/SimpleFooter.style.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+import { colorBlue, grid } from 'shared/constants';
+
+export const StyledContainer = styled.footer`
+  position: absolute;
+  bottom: ${grid * 2}px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const StyledTitle = styled.p`
+  margin-right: ${grid * 1.5}px;
+`;
+
+export const StyledLink = styled.a`
+  color: ${colorBlue};
+  margin-right: ${grid}px;
+`;
