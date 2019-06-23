3f7c483ce735003885571d56500a9440687597b7
Josowsky <bartosz@jozwowiak.pl>
2019-06-23 15:26:14 +0200

    Fix page building process

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,77 @@
 import React from 'react';
 import { shape } from 'prop-types';
 import { graphql } from 'gatsby';
-import SEO from 'components/SEO/SEO';
-
-import { routes } from 'shared/constants';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
 
 import {
-  StyledContainer,
-  StyledAvatarContainer,
-  StyledImage,
-  StyledHeader,
-  StyledButtons,
-  StyledButton,
-} from './index.style';
+  routes,
+  colorBlue,
+  colorGrey,
+  grid,
+  mobileBreakpoint,
+} from 'shared/constants';
+
+import Button from 'components/Button/Button';
+import SEO from 'components/SEO/SEO';
+
+const StyledContainer = styled.div`
+  margin-top: 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: ${mobileBreakpoint}px) {
+    margin-top: 200px;
+  }
+`;
+
+const StyledAvatarContainer = styled.div`
+  margin-bottom: ${grid * 1.5}px;
+
+  @media (min-width: ${mobileBreakpoint}px) {
+    display: none;
+  }
+`;
+
+const StyledImage = styled(Img)`
+  border-radius: 50%;
+  height: 110px;
+  width: 110px;
+  border: 1px solid ${colorBlue};
+`;
+
+const StyledHeader = styled.h1`
+  font-size: 24px;
+  color: ${colorGrey};
+  text-align: justify;
+
+  @media (min-width: ${mobileBreakpoint}px) {
+    font-size: 32px;
+    text-align: center;
+  }
+`;
+
+const StyledButtons = styled.div`
+  display: flex;
+  justify-content: stretch;
+  margin-top: ${grid * 2}px;
+  width: 100%;
+
+  @media (min-width: ${mobileBreakpoint}px) {
+    margin-top: ${grid * 3}px;
+    width: 400px;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  flex: 1 1 0%;
+  margin: ${grid}px;
+
+  @media (min-width: ${mobileBreakpoint}px) {
+    font-size: 18px;
+  }
+`;
 
 const MainPage = ({ data }) => (
   <StyledContainer>
--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -1,15 +1,37 @@
 import React, { Fragment } from 'react';
 import { shape } from 'prop-types';
 import { graphql } from 'gatsby';
-import SEO from 'components/SEO/SEO';
+import styled from 'styled-components';
+
+import { grid, mobileBreakpoint } from 'shared/constants';
 
 import PortfolioItem from 'components/PortfolioItem/PortfolioItem';
+import SEO from 'components/SEO/SEO';
 
-import {
-  StyledContainer,
-  StyledHeader,
-  StyledSubheader,
-} from './portfolio.style';
+const StyledContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledHeader = styled.h1`
+  font-size: 24px;
+  margin-top: ${grid * 4}px;
+  margin-bottom: ${grid}px;
+
+  @media (min-width: ${mobileBreakpoint}px) {
+    font-size: 32px;
+  }
+`;
+
+const StyledSubheader = styled.h2`
+  font-size: 16px;
+  margin-bottom: ${grid * 2}px;
+
+  @media (min-width: ${mobileBreakpoint}px) {
+    font-size: 24px;
+    margin-bottom: ${grid * 4}px;
+  }
+`;
 
 const PortfolioPage = ({ data }) => {
   const PROJECTS = [
