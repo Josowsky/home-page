35afe3fde30ae98d8a09cc010e6a7704c5374402
Josowsky <bartosz@jozwowiak.pl>
2019-07-18 19:45:32 +0200

    Add default image for meta

--- src/components/SEO/SEO.jsx
+++ src/components/SEO/SEO.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { shape, string } from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { Location } from '@reach/router';
+import { StaticQuery, graphql } from 'gatsby';
 
 const TITLE_TEMPLATE = '%s - Frontend Web Developer';
 
@@ -11,25 +12,42 @@ const SEO = ({
   imageUrl = '',
   location: { pathname },
 }) => (
-  <Helmet title={title} titleTemplate={TITLE_TEMPLATE}>
-    <html lang="en" />
-    <meta name="description" content={description} />
+  <StaticQuery
+    query={graphql`
+      query {
+        file(absolutePath: { regex: "/sidebar/" }) {
+          publicURL
+        }
+      }
+    `}
+    render={({ file: defaultImage }) => (
+      <Helmet title={title} titleTemplate={TITLE_TEMPLATE}>
+        <html lang="en" />
+        <meta name="description" content={description} />
 
-    <meta itemProp="name" content={title} />
-    <meta itemProp="description" content={description} />
-    {imageUrl && <meta itemProp="image" content={imageUrl} />}
+        <meta itemProp="name" content={title} />
+        <meta itemProp="description" content={description} />
+        <meta itemProp="image" content={imageUrl || defaultImage.publicURL} />
 
-    <meta property="og:url" content={pathname} />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={title} />
-    <meta property="og:description" content={description} />
-    {imageUrl && <meta property="og:image" content={imageUrl} />}
+        <meta property="og:url" content={pathname} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta
+          property="og:image"
+          content={imageUrl || defaultImage.publicURL}
+        />
 
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content={title} />
-    <meta name="twitter:description" content={description} />
-    {imageUrl && <meta name="twitter:image" content={imageUrl} />}
-  </Helmet>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta
+          name="twitter:image"
+          content={imageUrl || defaultImage.publicURL}
+        />
+      </Helmet>
+    )}
+  />
 );
 
 SEO.propTypes = {
