c027fe1173e2c609f0ab6be762f6701fd4837d4c
Josowsky <bartosz@jozwowiak.pl>
2019-10-06 03:03:29 +0200

    Refactor dependecies

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,28 +8,6 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   plugins: [
     `gatsby-plugin-layout`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
