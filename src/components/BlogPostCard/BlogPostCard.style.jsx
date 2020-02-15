0d762a257d2f04710fbf8952849a2fa96aded6cc
Josowsky <bartosz@jozwowiak.pl>
2020-02-15 14:54:20 +0100

    Refactor blog cards

--- src/components/BlogPostCard/BlogPostCard.style.jsx
+++ src/components/BlogPostCard/BlogPostCard.style.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+import { Heading, Paragraph } from 'components/Typography/Typography';
 import {
   colorBlue,
   colorGrey,
@@ -8,7 +9,7 @@ import {
 } from 'shared/constants/constants.style';
 
 export const StyledContainer = styled.article`
-  padding: ${grid * 2}px;
+  padding: ${grid}px;
   border: 1px rgba(119, 119, 119, 0.2) solid;
   box-shadow: 0 4px 4px rgba(119, 119, 119, 0.15);
   border-radius: 12px;
@@ -19,20 +20,21 @@ export const StyledContainer = styled.article`
 `;
 
 export const StyledHeader = styled.header`
-  margin-bottom: ${grid * 2}px;
+  margin-bottom: ${grid}px;
 `;
 
-export const StyledTitle = styled.h2`
+export const StyledTitle = styled(Heading)`
   color: ${colorBlue};
   margin: 0 0 ${grid}px;
   font-weight: ${fontWeightBold};
 `;
 
-export const StyledSmallDescription = styled.p`
+export const StyledSmallDescription = styled(Paragraph)`
   color: ${colorGrey};
   opacity: 0.6;
 `;
 
-export const StyledDescription = styled.p`
+export const StyledDescription = styled(Paragraph)`
   color: ${colorGrey};
+  margin: 0;
 `;
