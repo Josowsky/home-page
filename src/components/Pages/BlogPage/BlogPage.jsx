e2db49b4e4c3fe61374f1a7b70226dc582cb9d70
Josowsky <bartosz@jozwowiak.pl>
2020-03-06 22:26:47 +0100

    Wrap blog in blog wrapper

--- src/components/Pages/BlogPage/BlogPage.jsx
+++ src/components/Pages/BlogPage/BlogPage.jsx
@@ -7,6 +7,7 @@ import { getUrlPath } from 'shared/utils/getUrlPath';
 import Bio from 'components/Bio/Bio';
 import BlogPostCard from 'components/BlogPostCard/BlogPostCard';
 import SEO from 'components/SEO/SEO';
+import { StyledBlogContent } from 'components/PageWrappers/PageWrappers';
 import { StyledContainer, StyledBioContainer } from './BlogPage.style';
 
 const BlogPage = ({
@@ -14,29 +15,31 @@ const BlogPage = ({
     allContentfulPost: { edges: posts },
   },
 }) => (
-  <StyledContainer>
-    <StyledBioContainer>
-      <Bio />
-    </StyledBioContainer>
-    <div>
-      {posts.map(
-        ({ node: { id, title, description, createdAt, readingTime } }) => (
-          <BlogPostCard
-            key={id}
-            title={title}
-            description={description}
-            smallDescription={`${getBlogFormatDate(createdAt)}${readingTime &&
-              ` • ${readingTime} min read`}`}
-            path={`/blog/${getUrlPath(title)}`}
-          />
-        )
-      )}
-    </div>
-    <SEO
-      title="Blog"
-      description="On my blog, I write mostly about new frontend libraries and tools and everything related to web development."
-    />
-  </StyledContainer>
+  <StyledBlogContent>
+    <StyledContainer>
+      <StyledBioContainer>
+        <Bio />
+      </StyledBioContainer>
+      <div>
+        {posts.map(
+          ({ node: { id, title, description, createdAt, readingTime } }) => (
+            <BlogPostCard
+              key={id}
+              title={title}
+              description={description}
+              smallDescription={`${getBlogFormatDate(createdAt)}${readingTime &&
+                ` • ${readingTime} min read`}`}
+              path={`/blog/${getUrlPath(title)}`}
+            />
+          )
+        )}
+      </div>
+      <SEO
+        title="Blog"
+        description="On my blog, I write mostly about new frontend libraries and tools and everything related to web development."
+      />
+    </StyledContainer>
+  </StyledBlogContent>
 );
 
 BlogPage.propTypes = {
