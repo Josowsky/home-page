c2aebb0d81173f291d466b7c1a7b2e86b64e6ad7
Josowsky <bartosz@jozwowiak.pl>
2020-03-06 22:23:15 +0100

    Use single page layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,55 +1,26 @@
 import React from 'react';
-import { func, oneOfType, node, shape, oneOf } from 'prop-types';
+import { func, oneOfType, node } from 'prop-types';
 
-import Menu from 'components/Menu/Menu';
-import MobileSideBar from 'components/MobileSideBar/MobileSideBar';
-import Sidebar from 'components/Sidebar/Sidebar';
 import WithBorders from 'components/WithBorders/WithBorders';
 import SimpleFooter from 'components/SimpleFooter/SimpleFooter';
 
 import GlobalStyles from 'styles/GlobalStyles';
-import {
-  StyledContainer,
-  StyledMenu,
-  StyledSidebar,
-  StyledPageContent,
-  StyledBlogContent,
-} from './Layout.style';
+import { StyledContainer } from './Layout.style';
 
-const Layout = ({ children, pageContext }) => {
-  if (pageContext.layoutType === 'blog')
-    return (
-      <StyledContainer>
-        <GlobalStyles />
-        <WithBorders>
-          <StyledBlogContent>{children}</StyledBlogContent>
-          <SimpleFooter />
-        </WithBorders>
-      </StyledContainer>
-    );
-
+const Layout = ({ children }) => {
   return (
     <StyledContainer>
       <GlobalStyles />
       <WithBorders>
-        <StyledMenu>
-          <Menu />
-        </StyledMenu>
-        <StyledSidebar>
-          <Sidebar />
-        </StyledSidebar>
-        <StyledPageContent>{children}</StyledPageContent>
+        {children}
+        <SimpleFooter />
       </WithBorders>
-      <MobileSideBar />
     </StyledContainer>
   );
 };
 
 Layout.propTypes = {
   children: oneOfType([func, node]),
-  pageContext: shape({
-    layoutType: oneOf(['blog', 'home']).isRequired,
-  }).isRequired,
 };
 
 export default Layout;
