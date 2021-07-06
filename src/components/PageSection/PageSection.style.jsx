eb7316672fe2dfd4bfd70cd00b2b94d0f06d0287
Josowsky <bartosz@jozwowiak.pl>
2021-07-06 14:50:22 +0200

    [NP-7] Add margin-bottom to the PageSection

--- src/components/PageSection/PageSection.style.jsx
+++ src/components/PageSection/PageSection.style.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
+import { gutter } from 'shared/constants/constants.style';
+
 export const StyledContainer = styled.div`
   width: 100%;
   max-width: 1272px;
+  margin: 0 auto ${gutter(15)};
 `;
