630243e241b4224b56610b3d34174547f0e3a43e
Josowsky <bartosz@jozwowiak.pl>
2021-07-09 20:19:12 +0200

    [NP-6] Add PortfolioCardsSection to the new home page

--- src/components/NewHomePage/NewHomePage.jsx
+++ src/components/NewHomePage/NewHomePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import portfolioCardsSectionData from 'components/PortfolioCardsSection/lib/PortfolioCardsSection.data';
+
 import Grid, { Cell } from 'components/Grid/Grid';
 import PageSection from 'components/PageSection/PageSection';
 import HeroLogo from 'components/HeroLogo/HeroLogo';
 import HeroWithBanner from 'components/HeroWithBanner/HeroWithBanner';
+import PortfolioCardsSection from 'components/PortfolioCardsSection/PortfolioCardsSection';
 import TextWithCTABanner from 'components/TextWithCTABanner/TextWithCTABanner';
 
 // TODO: Move this component to "pages" directory after its done
@@ -16,6 +19,9 @@ const NewHomePage = () => (
         </Cell>
       </Grid>
       <HeroWithBanner />
+    </PageSection>
+    <PageSection>
+      <PortfolioCardsSection {...portfolioCardsSectionData} />
     </PageSection>
     <TextWithCTABanner />
   </main>
